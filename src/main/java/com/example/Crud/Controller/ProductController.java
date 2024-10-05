a1d8d2c8d8c47c3521a95977a56d1127eaf4c1a0
MiguelJeronimoOliveira <164900366+MiguelJeronimoOliveira@users.noreply.github.com>
2024-10-04 21:28:42 -0300

    Update: Controller
    Add: Get

--- src/main/java/com/example/Crud/Controller/ProductController.java
+++ src/main/java/com/example/Crud/Controller/ProductController.java
@@ -3,10 +3,14 @@ package com.example.Crud.Controller;
 import com.example.Crud.DTOS.RequestProduct;
 import com.example.Crud.Entities.Product;
 import com.example.Crud.Repositories.ProductRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.function.EntityResponse;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping
@@ -14,6 +18,14 @@ public class ProductController {
 
     @Autowired
     ProductRepository productRepository;
+
+    @GetMapping("/products")
+    public ResponseEntity AllProduts(){
+
+        var products = productRepository.findAll();
+
+        return ResponseEntity.ok(products);
+    }
 
 
 
