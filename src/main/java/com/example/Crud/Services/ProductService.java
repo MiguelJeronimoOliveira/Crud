da8199ca203c7b757912e6f0d1094bbdde792698
MiguelJeronimoOliveira <164900366+MiguelJeronimoOliveira@users.noreply.github.com>
2024-10-05 18:19:16 -0300

    Update: ProductService
    Add: Metodos de salvar, listar, procurar e deletar

--- src/main/java/com/example/Crud/Services/ProductService.java
+++ src/main/java/com/example/Crud/Services/ProductService.java
@@ -6,11 +6,38 @@ import com.example.Crud.Repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class ProductService {
 
     @Autowired
     ProductRepository productRepository;
+
+    public void SalvarProduto(RequestProduct product){
+        Product NovoProduto = new Product(product);
+        productRepository.save(NovoProduto);
+    }
+
+    public List<Product> ListarProdutos(){
+        List<Product> produtos = productRepository.findAll();
+
+        return produtos;
+    }
+
+
+    public Optional<Product> ProcurarPorId(UUID id){
+        Optional<Product> produto = productRepository.findById(id);
+
+        return  produto;
+    }
+
+    public void DeletarPorId(UUID id){
+        productRepository.deleteById(id);
+    }
+
 
 
 }
